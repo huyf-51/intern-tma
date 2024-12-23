f0188f5a79c1784d709edf4b51abe6bf0a3c1353
Trung Nguyen <141871306+vtrnguyen@users.noreply.github.com>
2024-12-23 17:48:55 +0700

    implement skeleton of orisoci's project

---
+++ orisoci_front_end/src/app/shared/components/child_component/child_component.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input, SimpleChanges} from '@angular/core';
+
+@Component({
+  selector: 'child-parent-component',
+  templateUrl: './child_component.component.html',
+  styleUrls: ['./child_component.component.scss']
+})
+export class ChildComponentComponent {
+
+}
